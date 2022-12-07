4aea580216371c7be6a9ed4b66d94dd68ce3f6d0
masonwilson <mew8wjn@virginia.edu>
2022-12-07 15:01:49 -0500

    and he jumps

--- ENGL-final-project/Assets/Scripts/PlayerMovement.cs
+++ ENGL-final-project/Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,17 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
     float horizontalMove = 0f;
     public float runSpeed = 40f;
+    bool jump = false;
 
     // Update is called once per frame
     void Update()
     {
         // gets arrows horizontally, -1 for left, 1 for right
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     void FixedUpdate()
@@ -22,7 +26,8 @@ public class PlayerMovement : MonoBehaviour
         // move our character here
         // param1: direction
         // param2: crouching?
-        // param3: jumping??
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        // param3: jumping?
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
