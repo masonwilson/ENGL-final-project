40b44c8c0a9f75389072e7ac62ad387274be46ba
masonwilson <mew8wjn@virginia.edu>
2022-12-07 14:52:00 -0500

    he runs omg

---
+++ ENGL-final-project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // gets arrows horizontally, -1 for left, 1 for right
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+    }
+
+    void FixedUpdate()
+    {
+        // move our character here
+        // param1: direction
+        // param2: crouching?
+        // param3: jumping??
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
